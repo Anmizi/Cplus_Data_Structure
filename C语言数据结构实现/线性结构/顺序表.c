d348a64085a443bc882a338798cb10bc3903c9d7
Anmizi <1845513904@qq.com>
2020-11-09 22:40:35 +0800

    添加顺序表

---
+++ C语言数据结构实现/线性结构/顺序表.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef int Position
+typeof struct LNode *List
+struct LNode{
+	ElementType Data[MaxSize];
+	Position Last;
+};
+//初始化
+List MakeEmpty(){
+	List L;
+	L = (List)malloc(sizeof(struct LNode));
+	L->Last = -1;
+	return L;
+} 
+//查找
+#define ERROR -1
+Position Find(List L,ElementType X){
+	Position i=0;
+	while(i<=L->Last&&L->Data[i]!=X)
+		i++;
+	if(i>L->Last) return ERROR;
+	else return i;
+} 
+
+//插入
+bool Insert(List L,ElementType X,Position P){
+	Position i;
+	if(L->Last == MaxSize -1){
+		printf("表满");
+		return false; 
+	}
+	if(P<0||P>L->Last+1){
+		printf("位置不合法");
+		return false;
+	}
+	for(i=L->Last;i>=P;i--){
+		L->Data[i+1] = L->Data[i];
+	}
+	L->Data[P] = X;
+	L->Last++;
+	return true;
+} 
+
+//删除
+bool Delete(List L,Position P){
+	Position i;
+	if(P<0||P>L->Last){
+		printf("位置%d不存在元素");
+		return false; 
+	}
+	for(i=P+1;i<L->Last;i++){
+		L->Data[i-1] = L->Data[i];
+	}
+	L->Last--;
+	return true;
+} 
+int main(){
+	
+	return 0;
+} 
